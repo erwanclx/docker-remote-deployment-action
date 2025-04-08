44ad575632a093b3acdafe725da8b274a471de48
erwanclx <81480765+erwanclx@users.noreply.github.com>
2025-04-08 18:30:45 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -81,10 +81,11 @@ docker compose -f "$STACK_FILE" pull
 
 echo "Deploying with: ${DEPLOYMENT_COMMAND} ${INPUT_ARGS} on staging context"
 
+docker compose -f "$STACK_FILE" ${INPUT_ARGS}
+
+
 # Check if optionnal additional commands are provided'
 if [ -n "${INPUT_ADDITIONAL_COMMANDS:-}" ]; then
   echo "Additional commands provided: ${INPUT_ADDITIONAL_COMMANDS}"
   eval ${INPUT_ADDITIONAL_COMMANDS}
 fi
-
-docker compose -f "$STACK_FILE" ${INPUT_ARGS}
